d32d07f8ba2b4ff341830805a8a595a1d1714848
Seth Zygarlicke <zygster11@gmail.com>
2021-06-30 13:43:59 -0500

    I think I changed known_good_state appropriately? Added my actual schema and test schema files to repo. Going to mess around with testing.

--- sql/field-agent-schema.sql
+++ sql/field-agent-schema.sql
@@ -1,6 +1,6 @@
-drop database if exists field_agent_test;
-create database field_agent_test;
-use field_agent_test;
+drop database if exists field_agent;
+create database field_agent;
+use field_agent;
 
 -- create tables and relationships
 create table agency (
@@ -14,7 +14,7 @@ create table location (
     `name` varchar(25) not null,
     address varchar(125) not null,
     city varchar(50) not null,
-    region varchar(25) null,
+	region varchar(25) null,
     country_code varchar(5) not null,
     postal_code varchar(15) not null,
     agency_id int not null,
@@ -96,64 +96,7 @@ create table alias (
         references agent(agent_id)
 );
 
-delimiter //
-create procedure set_known_good_state()
-begin
-
-	delete from location;
-    alter table location auto_increment = 1;
-    delete from agency_agent;
-	delete from agency;
-	alter table agency auto_increment = 1;
-    delete from agent;
-    alter table agent auto_increment = 1;
-    delete from security_clearance;
-    alter table security_clearance auto_increment = 1;
-    
-    insert into agency(agency_id, short_name, long_name) values
-        (1, 'ACME', 'Agency to Classify & Monitor Evildoers'),
-        (2, 'MASK', 'Mobile Armored Strike Kommand'),
-        (3, 'ODIN', 'Organization of Democratic Intelligence Networks');
-        
-	insert into location (location_id, name, address, city, region, country_code, postal_code, agency_id)
-		values
-	(1, 'HQ', '123 Elm', 'Des Moines', 'IA', 'USA', '55555', 1),
-    (2, 'Safe House #1', 'A One Ave.', 'Walla Walla', 'WA', 'USA', '54321-1234', 1),
-    (3, 'HQ', '123 Elm', 'Test', 'WI', 'USA', '55555', 2),
-	(4, 'Remote', '999 Nine St.', 'Test', 'WI', 'USA', '55555', 2),
-	(5, 'HQ', '123 Elm', 'Test', 'WI', 'USA', '55555', 3), -- for delete tests
-	(6, 'Remote', '999 Nine St.', 'Test', 'WI', 'USA', '55555', 3);
-        
-	insert into agent 
-		(first_name, middle_name, last_name, dob, height_in_inches) 
-	values
-		('Hazel','C','Sauven','1954-09-16',76),
-		('Claudian','C','O''Lynn','1956-11-09',41),
-		('Winn','V','Puckrin','1999-10-21',60),
-		('Kiab','U','Whitham','1960-07-29',52),
-		('Min','E','Dearle','1967-04-18',44),
-		('Urban','H','Carwithen',null,58),
-		('Ulises','B','Muhammad','2008-04-01',80),
-		('Phylys','Y','Howitt','1979-03-28',68);
-
-    insert into security_clearance values (security_clearance_id, name)
-        (1, 'Secret'), (2, 'Top Secret');
-        
-	insert into agency_agent 
-		(agency_id, agent_id, identifier, security_clearance_id, activation_date)
-    select
-        agency.agency_id,                              -- agency_id
-        agent.agent_id,                                -- agent_id
-        concat(agency.agency_id, '-', agent.agent_id), -- identifier
-        1,                                             -- security_clearance_id
-        date_add(agent.dob, interval 10 year)          -- activation_date
-    from agency
-    inner join agent
-    where agent.agent_id not in (6, 8)
-    and agency.agency_id != 2;
-
-end //
--- 4. Change the statement terminator back to the original.
-delimiter ;
-
 -- data
+insert into security_clearance values
+	(1, 'Secret'),
+    (2, 'Top Secret');
